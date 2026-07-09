df8d3abdace073feb90ed6b2333bdfb4641553f1
genai4usc-cloud <genai4usc@gmail.com>
2026-07-08 19:55:22 -0700

    Change default video length from 5 to 1

--- app/student/create-lecture/page.tsx
+++ app/student/create-lecture/page.tsx
@@ -48,7 +48,7 @@ export default function CreateLecture() {
   const [scriptDirect, setScriptDirect] = useState('');
   const [scriptFile, setScriptFile] = useState<File | null>(null);
   const [aiPrompt, setAiPrompt] = useState('');
-  const [videoLength, setVideoLength] = useState(5);
+  const [videoLength, setVideoLength] = useState(1);
   const [generatedScript, setGeneratedScript] = useState('');
   const [scriptGenerated, setScriptGenerated] = useState(false);
 
